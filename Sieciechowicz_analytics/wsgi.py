ad697707b33fc63f5ba2baff8b92fd587b5a8530
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-11-13 10:00:59 +0100

    Diagnogis Model

--- Sieciechowicz_analytics/wsgi.py
+++ Sieciechowicz_analytics/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Sieiechowicz_analytics project.
+WSGI config for Sieciechowicz_analytics project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sieiechowicz_analytics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sieciechowicz_analytics.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sieiechowicz_analytics.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sieciechowicz_analytics.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
