1d7bdca9fd3afa1b8f1b7fa30ffd58f53eda2426
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-06 13:59:35 +0100

    import to sql

--- Diagnosis/imports/importer.py
+++ Diagnosis/imports/importer.py
@@ -1,8 +1,15 @@
 import datetime
-
+import sys
 import pandas as pd
+from pandas import isnull
+import sqlite3
 
-diagnosis_df = pd.read_csv(r'C:\Users\Acer\Desktop\Sieciechowicz_analytics\Diagnosis\imports\2019_11_04.csv', index_col=False)
+basename = sys.argv[1]
+diagnosis_df = pd.read_csv(r'src\%s.csv' % basename)
+diagnosis_df = diagnosis_df.fillna('M')
+# diagnosis_df.to_string(formatters={'when':conv_date})
+# print(diagnosis_df)
+# exit()
 mmm2mm = {
     'sty': 1,
     'lut': 2,
@@ -17,21 +24,63 @@ mmm2mm = {
     'lis': 11,
     'gru': 12,
 }
-for index, row in diagnosis_df.iterrows():
-    when = row.when
-    when_parts = when.split(',')
-    # print(when_parts)
-    when_parts_1 = when_parts[0].split(' ')
-    # print(when_parts_1)
-    day = when_parts_1[0]
-    month = mmm2mm[when_parts_1[1]]
-    # print(day)
-    print(month)
-    when_parts_2 = when_parts[1].split(', ')
-    year = when_parts_2[0]
-    print(year)
 
-#  Brakuje rok, minuty i godziny, wyciagnac z pozostalych elementow to samo, jak zrobione z month oraz day. Potrzebne sa
-# 3 osobne zmienne, year hour minutes (zmienna ma sie nazywac hh, a minuty mm). Na koncu stworzyc obiekt datetime ktory
-# ma przyjac argumenty jako zmienne wyciagniete ze stringa.
+
+def conv_date(row):
+    when_parts = row.when.split(',')
+    when_parts_1 = when_parts[0].split(' ')
+    day = int(when_parts_1[0])
+    month = int(mmm2mm[when_parts_1[1]])
+    year = int(when_parts[1])
+    hour_and_minutes = when_parts[2].split(':')
+    hh = int(hour_and_minutes[0])
+    mm = int(hour_and_minutes[1])
+    dt = datetime.datetime(year, month, day, hh, mm)
+    return dt
+
+
+def set_m_ifnull(row):
+    sex = row.sex
+    if isnull(sex):
+        sex = 'M'
+    return sex
+
+
+def save_queries_into_file():
+    with open('res\%s.sql' % basename, 'w+') as sql_file:
+        for index, row in diagnosis_df.iterrows():
+            visit_dt = conv_date(row)
+            sex = set_m_ifnull(row)
+            # print(visit_dt, sex, row.icd10)
+            sql_file.write(
+                'INSERT INTO Diagnosis_diagnosis ("when", sex, icd10) VALUES (\'%s\', \'%s\', \'%s\');\r\n' % (
+                    visit_dt, sex, row.icd10))
+
+
+#save_queries_into_file()
+
+
+
+def save_to_database():
+    conn = sqlite3.connect('..\..\db.sqlite3')
+    with conn:
+        cur = conn.cursor()
+        for index, row in diagnosis_df.iterrows():
+            visit_dt = conv_date(row)
+            sex = set_m_ifnull(row)
+            sql = "INSERT INTO Diagnosis_diagnosis ('when', sex, icd10) VALUES (?, ?, ?)"
+            cur.execute(sql, (visit_dt, sex, row.icd10))
+            print(cur.lastrowid)
+
+# save_to_database()
+
+
+def save_to_database_2():
+    conn = sqlite3.connect('..\..\db.sqlite3')
+    with conn:
+        diagnosis_df.to_sql('Diagnosis_diagnosis', conn, index=False, if_exists='append')
+
+
+save_to_database_2()
+
 
