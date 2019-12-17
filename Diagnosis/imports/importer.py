975c853459ab90b0aaec98dcec6d12bf1893e93a
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2019-12-17 18:04:43 +0100

    before homework 17.12

--- Diagnosis/imports/importer.py
+++ Diagnosis/imports/importer.py
@@ -4,30 +4,38 @@ import sqlite3
 
 
 def save_to_database():
-    src_direct = 'src'
+    src_dir = 'src'
     conn = sqlite3.connect('..\..\db.sqlite3')
     with conn:
         cur = conn.cursor()
-        for file in src_direct:
-            if file.endswith('.csv'):
-                for index, row in file.iterrows():
-                    sql = "INSERT INTO Diagnosis_diagnosis ('when', sex, icd10) VALUES (?, ?, ?)"
-                    cur.execute(sql, (row.when, row.sex, row.icd10))
-
-
-def save_to_database_2():
-    src_dir = 'src'
-    con = sqlite3.connect('..\..\db.sqlite3')
-    with con:
         for entry in os.listdir(src_dir):
             if entry.endswith('.csv'):
                 csv_filepath = os.path.join(src_dir, entry)
                 diagnosis_df = pd.read_csv(csv_filepath)
                 diagnosis_df = diagnosis_df.fillna('M')
-                diagnosis_df.to_sql('Diagnosis_diagnosis', con, index=False, if_exists='append')
+                for index, row in diagnosis_df.iterrows():
+                    sql = "INSERT INTO Diagnosis_diagnosis ('when', sex, icd10) VALUES (?, ?, ?)"
+                    # cur.execute(sql, (row.when, row.sex, row.icd10))
+                os.rename(csv_filepath, csv_filepath.replace('.csv', '.imported'))
 
 
-save_to_database_2()
+save_to_database()
 
-# dostosowac kod z save_to_database by z kazdego pliku csv zczytywal, czyli ma robic to samo co save_to_database_2,
-# bo teraz dziala tylko dla jednego dataframe, poczytac o
+
+def save_to_database_2():
+    src_dir = 'src'
+    conn = sqlite3.connect('..\..\db.sqlite3')
+    with conn:
+        for entry in os.listdir(src_dir):
+            if entry.endswith('.csv'):
+                csv_filepath = os.path.join(src_dir, entry)
+                diagnosis_df = pd.read_csv(csv_filepath)
+                diagnosis_df = diagnosis_df.fillna('M')
+                diagnosis_df.to_sql('Diagnosis_diagnosis', conn, index=False, if_exists='append')
+
+
+# save_to_database_2()
+
+
+# os.listdir nie dziala, bo dziala tylko dla jednego poziomu, a chcemy wchodzic glebiej i glebiej, ta funkcja to os.walk.
+# Wykminic jak to zrobic, zastapic tak, by dzialalo
