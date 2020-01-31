a5daa150fa60240d0e788a19cecc71ab79e8fd4d
Marcin Sieciechowicz <sieciechowiczmarcin@wp.pl>
2020-01-31 16:48:17 +0100

    created new class CsvToDiagnosisImporter

--- Diagnosis/imports/base_diagnosis_importer.py
+++ Diagnosis/imports/base_diagnosis_importer.py
@@ -7,24 +7,21 @@ import pandas as pd
 
 
 class BaseCsvToTableImporter(ABC):
-    def __init__(self, src_dir: str, db_path: str):
+    def __init__(self, src_dir: str, db_path: str, target_table: str):
         self._check_src_dir(src_dir)
-        self._check_db_path(db_path)
-
         self.src_dir = src_dir
+
+        self._check_db_path(db_path)
         self.conn = self._init_conn(db_path)
 
-    @staticmethod
-    def _init_conn(db_path):
-        conn = None
-        try:
-            return sqlite3.connect(db_path)
+        self._check_target_table(target_table)
+        self.target_table = target_table
 
-        except sqlite3.Error as error:
-            cprint(f'ERR: {error}', 'grey', 'on_red')
-        finally:
-            if conn:
-                conn.close()
+    @staticmethod
+    def _check_src_dir(src_dir):
+        if not os.path.isdir(src_dir):
+            print(cprint('ERR: no source directory found', 'grey', 'on_red'))
+            exit()
 
     @staticmethod
     def _check_db_path(db_path):
@@ -33,9 +30,23 @@ class BaseCsvToTableImporter(ABC):
             exit()
 
     @staticmethod
-    def _check_src_dir(src_dir):
-        if not os.path.isdir(src_dir):
-            print(cprint('ERR: no source directory found', 'grey', 'on_red'))
+    def _init_conn(db_path):
+        conn = None
+        try:
+            return sqlite3.connect(db_path)
+        except sqlite3.Error as error:
+            cprint(f'ERR: {error}', 'grey', 'on_red')
+            exit()
+        finally:
+            if conn:
+                conn.close()
+
+    def _check_target_table(self, target_table):
+        cur = self.conn.cursor()
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name = ?"
+        cur.execute(sql, (target_table,))
+        if cur.fetchone() is None:
+            print(cprint(f'ERR: No target {target_table} table found', 'grey', 'on_red'))
             exit()
 
     def do_import(self):
@@ -46,9 +57,10 @@ class BaseCsvToTableImporter(ABC):
                         if file.endswith('.csv'):
                             csv_filepath = os.path.join(root, file)
                             self._save(self._get_dataframe(csv_filepath))
-                            self.rename_source_file(csv_filepath)
+                            self._rename_source_file(csv_filepath)
         finally:
-            self.conn.close()
+            if self.conn:
+                self.conn.close()
 
     @staticmethod
     def _get_dataframe(csv_filepath: str) -> pd.DataFrame:
@@ -57,12 +69,11 @@ class BaseCsvToTableImporter(ABC):
 
         return diagnosis_df
 
-    @abstractmethod
     def _save(self, diagnosis_df: pd.DataFrame):
-        pass
+        diagnosis_df.to_sql(self.target_table, self.conn, index=False, if_exists='append')
 
     @staticmethod
-    def rename_source_file(csv_filepath):
+    def _rename_source_file(csv_filepath):
         new_filename = csv_filepath.replace('.csv', '.imported')
         try:
             os.rename(csv_filepath, new_filename)
@@ -71,26 +82,12 @@ class BaseCsvToTableImporter(ABC):
             os.replace(csv_filepath, new_filename)
 
 
-class CsvToTableWholesaleImporter(BaseCsvToTableImporter):
+class CsvToDiagnosisImporter(BaseCsvToTableImporter):
 
-    def __init__(self, *, src_dir, db_path, target_table: str):
-        super().__init__(src_dir, db_path)
-        self._check_target_table(target_table)
-
-        self.target_table = target_table
-
-    def _check_target_table(self, target_table):
-        cur = self.conn.cursor()
-        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name = ?"
-        cur.execute(sql, (target_table,))
-        if cur.fetchone() is None:
-            print(cprint(f'ERR: No target {target_table} table found', 'grey', 'on_red'))
-            exit()
-
-    def _save(self, diagnosis_df):
-        diagnosis_df.to_sql(self.target_table, self.conn, index=False, if_exists='append')
+    def __init__(self, src_dir: str, db_path: str):
+        target_table = 'Diagnosis_diagnosis'
+        super().__init__(src_dir, db_path, target_table)
 
 
-wholesale_importer = CsvToTableWholesaleImporter(src_dir='src', db_path='..\..\db.sqlite3',
-                                                 target_table='Diagnosis_diagnosis')
-wholesale_importer.do_import()
+diagnosis_importer = CsvToDiagnosisImporter(src_dir='src', db_path='..\..\db.sqlite3')
+diagnosis_importer.do_import()
